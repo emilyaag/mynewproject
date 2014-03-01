3489926736c3393e02ca33b05fa490ca68fe03a0
Mike <Mike@Mike-PC>
2014-03-01 09:50:29 -0800

    This my test

---
+++ src/apples.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class apples {
+	 public static void main(String args[]){
+		
+		 Scanner input = new Scanner(System.in);
+		 tuna mytest = new tuna();
+		 System.out.println("Enter your name: ");
+		 String name = input.nextLine();
+         mytest.simpleMessage(name);
+ 
+         System.out.println("Enter your last name: ");
+		 String lname = input.nextLine();
+         mytest.simpleMessage(lname);
+ 
+	    // System.out.println("Hello youtube!");
+	    // double tuna;
+	    // tuna = 5.28;
+	    // System.out.println(tuna);
+		// Scanner bucky = new Scanner(System.in);
+		// double fnum,snum,answer;
+		// System.out.println("Enter first num: ");
+	  //   fnum = bucky.nextDouble();
+	  //   System.out.println(fnum);
+	 //    System.out.println("Enter second num:");
+	  //   snum = bucky.nextDouble();
+	 //    System.out.println(snum);
+	 //    answer=fnum+snum;
+	 //    System.out.println(answer);
+	     
+	    
+	  }
+
+}
---
+++ src/tuna.java
@@ -0,0 +1,7 @@
+
+public class tuna {
+		  public void simpleMessage(String name){
+		     System.out.println(name);
+		  }
+
+}
